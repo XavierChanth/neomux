4be0771d87b4184d9f548076b57043e1ae8494ad
xavierchanth <xchanthavong@gmail.com>
2024-10-07 00:28:41 -0400

    more examples

--- lua/main.lua
+++ lua/main.lua
@@ -2,5 +2,6 @@ local tmux = require("tmux")
 
 tmux.source({ "intable" })
 tmux.source("foo", "bar", "baz")
-
+tmux.source({ flags = "a", "posintable" })
+tmux.source({ "posintable", flags = "a" })
 tmux.print_config()
